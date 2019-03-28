9e45ca451a6329d5092765ca8ad72a1da76c2103
Raju-mac <rajushetty.b@gmal.com>
2019-03-29 02:09:58 +0530

    Ads changed

--- js/admob.js
+++ js/admob.js
@@ -4,16 +4,16 @@ var admobid = {};
 if( /(android)/i.test(navigator.userAgent) ) {
   admobid = { // for Android
     banner: '',
-    interstitial: 'ca-app-pub-6251426836814427/3272732399',
-    rewardvideo: 'ca-app-pub-6251426836814427/4960531197',
+    interstitial: 'ca-app-pub-3405799707672400/5561787757',
+    rewardvideo: 'ca-app-pub-3405799707672400/6659247009',
   };
-} else if(/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {
+} /*else if(/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {
   admobid = { // for iOS
     banner: '',
     interstitial: 'ca-app-pub-6251426836814427/7913997593',
     rewardvideo: 'ca-app-pub-6251426836814427/9390730792',
   };
-} /*else {
+} else {
   admobid = { // for Windows Phone
     banner: 'ca-app-pub-6869992474017983/8878394753',
     interstitial: 'ca-app-pub-6869992474017983/1355127956',
